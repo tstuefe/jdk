d04e0f4bc152baa38f34b06c36b40a0b10e07e59
tstuefe <thomas.stuefe@gmail.com>
2026-02-14 10:02:07 +0100

    add a way to simulate delays

--- src/java.base/unix/native/libjava/childproc.c
+++ src/java.base/unix/native/libjava/childproc.c
@@ -468,6 +468,15 @@ childProcess(void *arg)
 }
 
 #ifdef DEBUG
+/* This method is only used in debug builds */
+void jtregSimulateDelay() {
+    const char* env = getenv("JTREG_JSPAWNHELPER_DELAY_TEST");
+    if (env != NULL && atoi(env) == 1) {
+        printf("delay\n");
+        fflush(stdout);
+        sleep(1);
+    }
+}
 /* This method is only used in debug builds for testing MODE_POSIX_SPAWN
  * in the light of abnormal program termination of either the parent JVM
  * or the newly created jspawnhelper child process during the execution of
